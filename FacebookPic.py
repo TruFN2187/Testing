c70ea78486392d1a03dddb2718fe50fdef133dd7
TruFN2187 <grant.mlanier@gmail.com>
2016-10-24 19:15:47 -0500

    Create FacebookPic.py

---
+++ FacebookPic.py
@@ -0,0 +1,21 @@
+url =#String version of a facebook profile
+os.makedirs('TestFacebook', exist_ok=True)
+
+res = requests.get(url)
+res.raise_for_status()
+
+soup = bs4.BeautifulSoup(res.text)
+imgElems = soup.select('img')
+
+if imgElems == []:
+    print('Nothing Here!')
+else:
+    imgUrl = 'https://' + imgElems[0].get('src')
+
+    res = requests.get(imgUrl)
+    res.raise_for_status()
+    faceFile = open(os.path.join('TestFacebook', os.path.basename(imgUrl)), 'wb')
+    for chunk in res.iter_content(100000):
+        faceFile.write(chunk)
+    faceFile.close()
+    
